f016bfb60c0b376d9062eae20738c095fc93e8d0
Saket kumar <46194436+saketkum11@users.noreply.github.com>
2021-11-16 12:06:15 +0530

    update app js

--- src/App.js
+++ src/App.js
@@ -2,19 +2,51 @@ import "./styles.css";
 import React, { useState } from "react";
 
 export default function App() {
-  var [bithdate, setBirthDate] = useState("");
+  var [birthdate, setBirthDate] = useState("");
   var [lucky, setLuckyNumber] = useState(0);
   function birthdayHandler(event) {
-    setLuckyNumber(event.target.value);
+    setBirthDate(event.target.value);
+  }
+  function luckyHandler(e) {
+    setLuckyNumber(e.target.value);
+  }
+  function calculateBirth(date) {
+    var sum = 0;
+    var replace = date.replaceAll("-", "");
+
+    for (var i = 0; i < replace.length; i++) {
+      sum = sum + Number(replace.charAt(i));
+    }
+    return sum;
+  }
+  function submitHandler() {
+    var birth = calculateBirth(birthdate);
+
+    setTimeout(() => {
+      setLuckyNumber(birth);
+      console.log(birth);
+    }, 1000);
   }
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Let see your Birthdate is Lucky?</h1>
+
       <label style={{ display: "block" }}>Birthday </label>
-      <input onChange={birthdayHandler}></input>
+      <input
+        style={{ textAlign: "center" }}
+        type="date"
+        onChange={birthdayHandler}
+      ></input>
       <label style={{ display: "block" }}>Lucky Number</label>
-      <input onChange={birthdayHandler}></input>
+      <input
+        type="number"
+        style={{ textAlign: "center" }}
+        onChange={luckyHandler}
+      ></input>
+      <button style={{ display: "block" }} onClick={submitHandler}>
+        Sumbit
+      </button>
+      <h1>{birthdate}</h1>
       <h1>{lucky}</h1>
     </div>
   );
