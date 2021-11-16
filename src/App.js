bcf3e5ed4f495a9f468f419e94e15c4fc987d9f7
Saket kumar <46194436+saketkum11@users.noreply.github.com>
2021-11-16 11:28:38 +0530

    update input ,usestate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./styles.css";
-
+import React, { useState } from "react";
 
 export default function App() {
+  var [bithdate, setBirthDate] = useState("");
+  var [lucky, setLuckyNumber] = useState(0);
+  function birthdayHandler(event) {
+    setLuckyNumber(event.target.value);
+  }
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <label style={{ display: "block" }}>Birthday </label>
+      <input onChange={birthdayHandler}></input>
+      <label style={{ display: "block" }}>Lucky Number</label>
+      <input onChange={birthdayHandler}></input>
+      <h1>{lucky}</h1>
     </div>
   );
 }
